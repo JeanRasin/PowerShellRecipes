d8df3ce74d7e17a113fccfb91ddac5a8c878132d
DESKTOP-9D6870A\Jean <logun2@yandex.ru>
2021-03-03 14:54:13 +0500

    Исправление генерации названия коммита и форматирование скрипта

--- Git hook commit-msg автоматические подстановки к названию коммита.ps1
+++ Git hook commit-msg автоматические подстановки к названию коммита.ps1
@@ -1,9 +1,3 @@
-Что бы коммиты имели правильное название можно задать шаблон, например: BMW_FRONT-985 - Изменение главной страницы.
-В папке .git\hooks локально или глобально нужно удалить файл commit-msg.sample и сделать commit-msg. В нем прописать:
-#!/bin/sh
-c:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -ExecutionPolicy RemoteSigned -Command '.git\hooks\commit-msg.ps1' $1
-Это нужно для запуска PowerShell и выполнение команд в файле commit-msg.ps1, который то же нужно создать в этой папке и в нем прописать код ниже.
-Этот код берет префикс $ProjectName-<number> из имени ветки и автоматически добавляет к названию коммита который вводится. Так же делает название коммита с заглавной буквы.
 [CmdletBinding()]
 param (
   [Parameter()]
@@ -12,26 +6,27 @@ param (
 )
 
 # Конвертер кодировок.
-   function ConvertTo-Encoding ([string]$From, [string]$To){
-        Begin{
-            $encFrom = [System.Text.Encoding]::GetEncoding($from)
-            $encTo = [System.Text.Encoding]::GetEncoding($to)
-        }
-        Process{
-            $bytes = $encTo.GetBytes($_)
-            $bytes = [System.Text.Encoding]::Convert($encFrom, $encTo, $bytes)
-            $encTo.GetString($bytes)
-        }
-    }
+function ConvertTo-Encoding ([string]$From, [string]$To) {
+  Begin {
+    $encFrom = [System.Text.Encoding]::GetEncoding($from)
+    $encTo = [System.Text.Encoding]::GetEncoding($to)
+  }
+  Process {
+    $bytes = $encTo.GetBytes($_)
+    $bytes = [System.Text.Encoding]::Convert($encFrom, $encTo, $bytes)
+    $encTo.GetString($bytes)
+  }
+}
 
 $CommitMessage = Get-Content -Path $CommitFile
-$ProjectName = "BMW_FRONT"
+$ProjectName = "TRACKERX"
 
 $CommitMessage = $CommitMessage | ConvertTo-Encoding "UTF-8" "windows-1251"
 Write-Host $CommitMessage
+
 if ($CommitMessage -match '\S.') {
 
-  $CommitMessage = (Get-Culture).TextInfo.ToTitleCase($CommitMessage)
+  $CommitMessage = $CommitMessage.substring(0, 1).ToUpper() + $CommitMessage.Remove(0, 1)
   $CurrentBranch = (git rev-parse --abbrev-ref HEAD)
   $HasMatch = $CurrentBranch -match "(.+_" + $ProjectName + "-)(\d+)"
 
